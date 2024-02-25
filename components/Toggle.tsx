048ce6cbd4f4572ece08238000122f5f76d2b71d
Fatlir Topalli <fatlir-t@outlook.com>
2024-02-25 23:51:11 +0000

    feat: change active styles for dark mode toggle

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -11,7 +11,7 @@ const Toggle = ({ defOption, altOption }: { defOption: string; altOption: string
 
     return (
 
-        <div onClick={handleToggleClick} className="hover:cursor-pointer hover:bg-slate-900/50 dark:hover:bg-neutral-900/80 active:bg-[#252a30ab] active:border-2 active:m-[-2px] toggle-container px-3 py-2 text-sm font-medium flex">
+        <div onClick={handleToggleClick} className="hover:cursor-pointer hover:bg-slate-900/50 dark:hover:bg-neutral-900/80 active:bg-[#252a30ab] active:border-2 active:border-white border-2 border-transparent toggle-container px-3 py-2 text-sm font-medium flex w-fit">
             <div className={`toggle-switch hover:bg-white active:bg-[#d5d5d5] active:m-[-2px] active:border-2 ${isDefault ? '' : 'alt'}`}></div>
             <div>
                 <label className="toggle-label hover:cursor-pointer">{defOption} {altOption}</label>
