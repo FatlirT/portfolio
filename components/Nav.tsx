da672f8fe69ae6ed9ef2982bab90fdcf2ad920f9
Fatlir Topalli <fatlir-t@outlook.com>
2024-02-25 23:51:38 +0000

    fix: add styled and functioning mobile menu

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,8 +1,15 @@
+'use client';
 import Image from "next/image";
 import Toggle from "./Toggle";
+import { useState } from "react";
 
 const Nav = () => {
 
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleToggleClick = () => {
+        setIsOpen(!isOpen);
+    };
 
     return (
         <>
@@ -12,7 +19,7 @@ const Nav = () => {
 
                     <div className="items-center flex">
                         <div className="flex flex-shrink-0 items-center ">
-                            <a href="/"><Image className="h-8 w-auto drop-shadow-[0.1rem_0_0.25rem_#00011099]" src="/images/ft.png" alt="Your Company"
+                            <a href="/"><Image className="h-8 w-auto drop-shadow-[0.1rem_0_0.25rem_#00011099]" src="/images/ft.png" alt="Fatlir Topalli logo"
                                 width="100" height="100" />
                             </a>
                         </div>
@@ -25,22 +32,20 @@ const Nav = () => {
                         </div>
                     </div>
 
-                    <Toggle defOption="☀️" altOption="🌙"></Toggle>
+                    <div className="hidden sm:block"><Toggle defOption="☀️" altOption="🌙"></Toggle></div>
+
 
                     <div className="inset-y-0 left-0 flex items-center sm:hidden">
 
                         {/* <!-- Mobile menu button--> */}
-                        <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                        <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 hover:bg-slate-900/50 dark:hover:bg-neutral-900/80 active:bg-[#252a30ab] active:border-2 active:m-[-2px] focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" onClick={handleToggleClick} aria-controls="mobile-menu" aria-expanded="false">
                             <span className="absolute -inset-0.5"></span>
 
                             {/* <!-- Icon when menu is closed. Menu open: "hidden", Menu closed: "block" --> */}
-                            <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                            </svg>
-
-                            {/* <!-- Icon when menu is open. Menu open: "block", Menu closed: "hidden" --> */}
-                            <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            <svg className={`mob-menu icon ${isOpen ? "close" : ""} h-6 w-6 mob-menu`} fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                <path className="mob-menu icon bar" stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75" />
+                                <path className="mob-menu icon bar" stroke-linecap="round" stroke-linejoin="round" d="M3.75 12h16.5m-16.5" />
+                                <path className="mob-menu icon bar" stroke-linecap="round" stroke-linejoin="round" d="M3.75 17.25h16.5" />
                             </svg>
                         </button>
                     </div>
@@ -48,13 +53,16 @@ const Nav = () => {
             </div>
 
             {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-            {/* <div className="sm:hidden" id="mobile-menu">
-                    <div className="space-y-1 px-2 pb-3 pt-2"> */}
-            {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-            {/* <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About me</a>
-                        <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Projects</a>
+            <div className={`sm:hidden ${isOpen ? "block" : "hidden"}`} id="mobile-menu">
+                <div className="space-y-1 px-2 pb-3 pt-2">
+                    {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+                    <a href="#" className="w-full hover:bg-slate-900/50 dark:hover:bg-neutral-900/80 active:bg-[#252a30ab] active:border-2 active:border-white border-transparent border-2 box-border block rounded-md px-3 py-2 text-base font-medium active:text-neutral-200">About me</a>
+                    <a href="#" className="w-full hover:bg-slate-900/50 dark:hover:bg-neutral-900/80 active:bg-[#252a30ab] active:border-2 active:border-white border-transparent border-2 box-border block rounded-md px-3 py-2 text-base font-medium active:text-neutral-200" aria-current="page">Projects</a>
+                    <div className="sm:hidden px-3 py-2">
+                        <Toggle defOption="☀️" altOption="🌙"></Toggle>
                     </div>
-                </div> */}
+                </div>
+            </div>
         </>
     );
 
