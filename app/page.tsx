5e57dfa7576ccc2b089b02f8482049ded2e909fb
Fatlir Topalli <fatlir-t@outlook.com>
2024-02-25 20:28:21 +0000

    feat: remove glow from behind logo on light mode

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
               <h1 className='text-xl'>enthusiast!</h1>
             </div>
           </div>
-          <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1] mb-16">
+          <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic   after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-sky-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0180ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1] mb-16">
             <Image
               className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
               src="/images/ft.png"
@@ -39,7 +39,7 @@ export default function Home() {
 
 
 
-        {/* <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
 
           {
             projects.map((project) =>
@@ -50,7 +50,10 @@ export default function Home() {
                 <h4>{project.webApp}</h4>
               </div>
             )}
-        </div> */}
+        </div>
+
+
+
       </main >
     </>
   );
