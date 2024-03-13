93c118118c46ed0d44131ced9d9a55d70cd99ffd
Fatlir Topalli <fatlir-t@outlook.com>
2024-03-13 14:07:51 +0000

    feat: add boolean defPref prop to set initial value to either def or alt

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -1,9 +1,9 @@
 'use client';
 import { useState } from "react";
 
-const Toggle = ({ defOption, altOption }: { defOption: string; altOption: string; }) => {
+const Toggle = ({ defPref, defOption, altOption }: { defPref: boolean; defOption: string; altOption: string; }) => {
 
-    const [isDefault, setIsDefault] = useState(true);
+    const [isDefault, setIsDefault] = useState(defPref);
 
     const handleToggleClick = () => {
         setIsDefault(!isDefault);
@@ -11,8 +11,8 @@ const Toggle = ({ defOption, altOption }: { defOption: string; altOption: string
 
     return (
 
-        <div onClick={handleToggleClick} className="hover:cursor-pointer hover:bg-slate-900/50 dark:hover:bg-neutral-900/80 active:bg-[#252a30ab] active:border-2 active:border-white border-2 border-transparent toggle-container px-3 py-2 text-sm font-medium flex w-fit">
-            <div className={`toggle-switch hover:bg-white active:bg-[#d5d5d5] active:m-[-2px] active:border-2 ${isDefault ? '' : 'alt'}`}></div>
+        <div onClick={handleToggleClick} className="hover:cursor-pointer hover:bg-dark-acc/20 active:bg-dark-acc/90 active:border-2 active:border-white border-2 border-transparent toggle-container bg-dark-acc/60 px-3 py-2 text-sm font-medium flex w-fit">
+            <div className={`toggle-switch bg-light-acc active:bg-light-acc active:m-[-2px] active:border-2 ${isDefault ? '' : 'alt'}`}></div>
             <div>
                 <label className="toggle-label hover:cursor-pointer">{defOption} {altOption}</label>
             </div>
