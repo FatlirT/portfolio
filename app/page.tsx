811094857432203ec5942820b38795386909228c
Fatlir Topalli <fatlir-t@outlook.com>
2024-03-13 14:10:09 +0000

    feat: add anchors

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import fields from '@/assets/fields';
 import projects from '@/assets/projects';
+import Footer from '@/components/Footer';
+import Nebula from '@/components/Nebula';
 import Splash from '@/components/Splash';
 import Typewriter from '@/components/Typewriter';
 import Image from 'next/image';
@@ -12,11 +14,11 @@ export default function Home() {
   return (
     <>
       <Splash></Splash>
-      <main className="flex flex-col items-center justify-between w-full max-w-7xl mx-auto px-16 mb-16">
+      <main id="main" className="drop-shadow-2xl overflow-visible flex flex-col items-center justify-between w-full max-w-7xl mx-auto px-16 mb-16">
 
         <div className="flex flex-row justify-between w-full items-center">
           <div className='w-full flex-col'>
-            <h1 className="text-5xl font-black w-full justify-start flex">Hey! <span id="wave">👋</span></h1><br /><br /><br className="" />
+            <h1 className="text-5xl font-black w-full justify-start flex">Hey! <span>👋</span></h1><br /><br /><br className="" />
             <div className="w-full mb-16">
               <div className="pb-2">
                 <h1 className="inline-block text-xl">I&apos;m </h1><h1 className='font-bold inline-block text-xl pl-[7px]'>@</h1><h1 className="inline-block text-2xl font-bold pr-[7px]">FatlirT,</h1><h1 className="inline-block text-xl">the</h1>
@@ -25,22 +27,20 @@ export default function Home() {
               <h1 className='text-xl'>enthusiast!</h1>
             </div>
           </div>
-          <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic   after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-sky-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0180ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1] mb-16">
-            <Image
-              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-              src="/images/ft.png"
-              alt="Next.js Logo"
-              width={360}
-              height={74}
-              priority
-            />
+          <div className="relative">
+            <div className='absolute'>
+              <div className="absolute">
+                <Nebula></Nebula>
+              </div>
+            </div>
           </div>
         </div>
+      </main >
 
-
-
+      <div id="projects" className="w-full flex flex-col mb-16 justify-center items-center pt-32">
+        <h1 className='text-4xl font-bold mb-16'>Projects</h1>
+        <Nebula></Nebula>
         <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-
           {
             projects.map((project) =>
               <div key={project.name}>
@@ -51,10 +51,24 @@ export default function Home() {
               </div>
             )}
         </div>
+      </div>
 
 
+      <div id="about" className="w-full flex flex-col mb-16 justify-center items-center pt-32">
+        <h1 className='text-4xl font-bold mb-16'>About me</h1>
+        <Nebula></Nebula>
+        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+          <p>ABOUTME</p>
+        </div>
+      </div>
 
-      </main >
+      <div id="featured-articles" className="w-full flex flex-col mb-16 justify-center items-center pt-32">
+        <h1 className='text-4xl font-bold mb-16'>Featured Articles</h1>
+        <Nebula></Nebula>
+        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+          <p>ABOUTME</p>
+        </div>
+      </div>
     </>
   );
 }
