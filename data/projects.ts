1fca10acc22f6476181a13a39d8b2834a0a5a66c
Fatlir Topalli <fatlir-t@outlook.com>
2024-03-15 15:28:49 +0000

    chore: rename assets to data

--- data/projects.ts
+++ data/projects.ts
@@ -3,14 +3,14 @@ const projects = [
         name: "FatlirT Portfolio",
         description: "This site!",
         tech: ["next.js", "tailwind", "express.js", "aws", "terraform"],
-        sourceCode: "www.github.com",
+        sourceCode: "www.github.com/FatlirT/portfolio",
         webApp: "fatlirt.vercel.app",
     },
     {
         name: "Elleo - Website for Elleo LTD",
         description: "Elleo LTD, a conceptual electrical services company.",
         tech: ["next.js", "tailwind", "express.js", "aws", "terraform"],
-        sourceCode: "www.github.com",
+        sourceCode: "www.github.com/FatlirT/elleo",
         webApp: "elleo.vercel.app",
     },
 ];
