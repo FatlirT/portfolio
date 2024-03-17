f787277affb85e26e3f8f9036d3383253d21848c
Fatlir Topalli <fatlir-t@outlook.com>
2024-03-17 16:12:12 +0000

    feat: change Toggle styling

--- components/Toggle.tsx
+++ components/Toggle.tsx
@@ -14,8 +14,8 @@ const Toggle = ({ onClick, mode, defOption, altOption }: { onClick: Function; mo
 
     return (
 
-        <div onClick={handleToggleClick} className="hover:cursor-pointer hover:bg-dark-acc/20 active:bg-dark-acc/90 active:border-2 active:border-white border-2 border-transparent toggle-container bg-dark-acc/60 px-3 py-2 text-sm font-medium flex w-fit">
-            <div className={`toggle-switch bg-light-acc active:bg-light-acc active:m-[-2px] active:border-2 ${mode === defOption.name ? '' : 'alt'}`}></div>
+        <div onClick={handleToggleClick} className="hover:cursor-pointer hover:bg-dark-acc/20 active:bg-dark-acc/70 active:border-2 active:border-white border-2 border-transparent toggle-container bg-dark-acc/50 px-3 py-2 text-sm font-medium flex w-fit">
+            <div className={`toggle-switch bg-light-acc active:m-[-2px] active:border-2 ${mode === defOption.name ? '' : 'alt'}`}></div>
             <div>
                 <label className="toggle-label hover:cursor-pointer">{defOption.symbol} {altOption.symbol}</label>
             </div>
