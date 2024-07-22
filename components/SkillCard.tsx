76b96f7dc62d3537b1ce5ae0eddb06bf9e79b36d
Fatlir Topalli <fatlir-t@outlook.com>
2024-07-22 20:51:54 +0100

    feat: add skill card component to display information on individual skills

---
+++ components/SkillCard.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import { ImgHTMLAttributes } from "react";
+
+interface props extends ImgHTMLAttributes<HTMLImageElement> {
+    className: string,
+    skillImageUrl: string,
+    skillName: string;
+}
+
+
+const SkillCard = ({ skillImageUrl, skillName, className }: props) => {
+    return (
+        <>
+            <div className={className + " shadow-lg shadow-black/30 bg-light-acc/35 rounded-3xl flex flex-col text-center items-center p-8 text-2xl hover:bg-light-acc/45 active:bg-light-acc/25 cursor-pointer"}>
+                <div className="image-container flex flex-col justify-center h-full items-center text-center center pb-5">
+                    <Image
+                        src={skillImageUrl}
+                        width={500}
+                        height={500}
+                        alt="Picture of skill"
+                    />
+                </div>
+                <div className="caption-container items-center text-center flex flex-col font-black">{skillName}</div>
+            </div >
+        </>
+    );
+};
+
+export default SkillCard;
