03f3f6b716d58205a2f0c12e47c867705c3fb5f1
Fatlir Topalli <fatlir-t@outlook.com>
2024-08-25 21:31:09 +0100

    feat: add better styling to skill cards Better height and width limiting of image by setting maxWidth and maxHeight as-well as specifying objectFit to contain the image; that way, we will always have an image which conforms to the aspect ratio and size limits defined in the container and image css.

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -13,12 +13,13 @@ const SkillCard = ({ skillImageUrl, skillName, skillFuture, className }: props) 
     return (
         <>
             <div className={className + " shadow-lg shadow-black/30 bg-light-acc/35 rounded-3xl flex flex-col text-center items-center p-8 text-2xl " + (skillFuture ? 'saturate-0 opacity-70' : 'hover:bg-light-acc/45 active:bg-light-acc/25 cursor-pointer')}>
-                <div className="image-container flex flex-col justify-center h-full items-center text-center center pb-5">
+                <div className="image-container w-40 h-40 flex flex-col justify-center items-center text-center center pb-5">
                     <Image
                         src={skillImageUrl}
                         width={500}
                         height={500}
                         alt="Picture of skill"
+                        style={{ objectFit: "contain", maxWidth: '75%', maxHeight: '75%' }}
                     />
                 </div>
                 <div className="caption-container items-center text-center flex flex-col font-black">{skillName}</div>
