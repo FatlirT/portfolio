06c09c51d7b74d4b428908c927342c5f2e151328
Fatlir Topalli <fatlir-t@outlook.com>
2024-08-31 20:59:26 +0100

    feat: add WIP hazard tape

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -19,8 +19,13 @@ interface props extends ImgHTMLAttributes<HTMLImageElement> {
 const ProjectCard = ({ name, description, imageUrls, tech, wip, className, soureCodeUrl, url }: props) => {
     return (
         <>
-            <div className={className + " shadow-lg shadow-black/30 bg-light-acc/35 rounded-3xl flex flex-row text-center items-center p-8 text-2xl"}>
-                <div>{wip ? "wip" : ""}</div>
+            <div className={className + " overflow-clip shadow-lg shadow-black/30 bg-light-acc/35 rounded-3xl flex flex-row text-center items-center p-8 text-2xl active:opacity-80 cursor-pointer relative"}>
+                <div id='wip-hazard-tape' className={(wip ? "" : "hidden") + " overflow-clip bg-black top-2 shadow-gray-800 shadow-lg -left-16 absolute -rotate-45 w-60 h-12"}>
+                    <div className='bg-yellow-300 w-10 rotate-12 h-20 absolute top-0 left-0' ></div>
+                    <div className='bg-yellow-300 w-10 rotate-12 h-20 absolute -top-2 left-1/3' ></div>
+                    <div className='bg-yellow-300 w-10 rotate-12 h-20 absolute -top-2 left-2/3' ></div>
+                    <div className='bg-white/10 shadow-white/5 shadow-xl w-60 h-20 absolute -top-16 left-0'></div>
+                </div>
                 {name}
                 <div className="image-container flex flex-col justify-center h-full items-center text-center center pb-5">
                     {imageUrls}
