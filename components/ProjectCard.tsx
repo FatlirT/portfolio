933a9ea033b5187b7eb42f4f891d5c8700b99a0f
Fatlir Topalli <fatlir-t@outlook.com>
2024-09-23 21:53:30 +0100

    feat: improve responsive design of projects

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -19,14 +19,14 @@ interface props extends ImgHTMLAttributes<HTMLImageElement> {
 const ProjectCard = ({ name, description, imageUrls, tech, wip, className, soureCodeUrl, url }: props) => {
     return (
         <>
-            <div className={"w-full max-w-full overflow-clip text-wrap shadow-lg shadow-dark-acc/30 bg-light-acc/35 rounded-3xl flex flex-row text-center items-center p-8 text-2xl active:opacity-80 cursor-pointer relative " + className}>
-                <div id='wip-hazard-tape' className={(wip ? "" : "hidden") + " overflow-clip bg-black top-2 shadow-slate-700/70 shadow-lg -left-16 absolute -rotate-45 w-60 h-12 p-0"}>
+            <div className={"w-full max-w-full overflow-clip text-wrap shadow-lg shadow-dark-acc/30 bg-light-acc/35 rounded-3xl flex flex-row text-center items-center p-8 text-xl sm:text-2xl active:opacity-80 cursor-pointer relative " + className}>
+                <div id='wip-hazard-tape' className={(wip ? "" : "hidden") + " overflow-clip bg-black top-2 shadow-slate-700/70 shadow-lg -left-16 absolute text-center justify-center items-center sm:text-2xl text-[1rem] -rotate-45 w-60 h-9 sm:h-12"}>
 
-                    <div className='bg-yellow-300 w-9 rotate-[22.5deg] h-20 absolute top-0 left-1' ></div>
+                    <div className='bg-yellow-300 w-9 rotate-[22.5deg] h-20 absolute -top-1 left-1' ></div>
                     <div className='bg-yellow-300 w-9 rotate-[22.5deg] h-20 absolute -top-3 left-[35%] -z-30' ></div>
                     <div className='bg-yellow-300 w-9 rotate-[22.5deg] h-20 absolute -top-4 left-2/3' ></div>
                     <div className='bg-white/10 shadow-white/5 shadow-xl blur w-60 h-20 absolute -top-16 left-0'></div>
-                    <h1 className='absolute top-2 left-20 italic font-black drop-shadow-[0_1.5px_6px_rgba(0,0,0,0.8)]'>WIP</h1>
+                    <h1 className='absolute sm:top-2 top-1 left-20 italic font-black drop-shadow-[0_1.5px_6px_rgba(0,0,0,0.8)]'>WIP</h1>
                 </div>
                 {name}
                 <div className="image-container flex flex-col justify-center h-full items-center text-center center pb-5">
