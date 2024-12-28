8c2e841cfe8a02c84234968c907fd69965f1d0fc
Fatlir Topalli <fatlir-t@outlook.com>
2024-12-28 01:13:14 +0000

    feat: responsive project card - no background on below sm screens - new skill image grid layout

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -19,8 +19,8 @@ interface props {
 const ProjectCard = ({ name, description, imageUrls, tech, wip, soureCodeUrl, url }: props) => {
     return (
         <>
-            <div id="project-card-main" className="w-full max-w-full overflow-clip text-wrap shadow-lg shadow-dark-acc/30 bg-light-acc/35 rounded-3xl p-8 text-xl relative flex flex-col items-center text-center justify-center space-y-6 space-y-reverse">
-                <div id='wip-hazard-tape' className={(wip ? "" : "hidden") + " overflow-clip bg-black top-2 shadow-slate-700/70 shadow-lg -left-16 absolute text-center justify-center items-center sm:text-2xl text-[1rem] -rotate-45 w-60 h-9 sm:h-12"}>
+            <div id="project-card-main" className="w-full max-w-full overflow-clip text-wrap sm:shadow-lg shadow-dark-acc/30 sm:bg-light-acc/35 rounded-3xl sm:p-8 text-xl relative flex flex-col items-center text-center justify-center space-y-6 space-y-reverse">
+                <div id='wip-hazard-tape' className={(wip ? "" : "hidden") + " overflow-clip bg-black top-2 sm:shadow-slate-700/70 shadow-lg -left-16 absolute text-center justify-center items-center sm:text-2xl text-[1rem] -rotate-45 w-60 h-9 sm:h-12"}>
                     <div className='bg-yellow-300 w-9 rotate-[22.5deg] h-20 absolute -top-1 left-1' ></div>
                     <div className='bg-yellow-300 w-9 rotate-[22.5deg] h-20 absolute -top-3 left-[35%] -z-30' ></div>
                     <div className='bg-yellow-300 w-9 rotate-[22.5deg] h-20 absolute -top-4 left-2/3' ></div>
@@ -31,7 +31,7 @@ const ProjectCard = ({ name, description, imageUrls, tech, wip, soureCodeUrl, ur
                 <div id="project-card-top" className="font-black text-4xl pb-6 pt-4 underline ">
                     {name}
                 </div>
-                <div id="project-card-bot" className="w-full flex-col flex lg:flex-row justify-between space-y-6 px-8 lg:space-x-8">
+                <div id="project-card-bot" className="w-full flex-col flex lg:flex-row justify-between space-y-6 md:px-8 lg:space-x-8">
                     <div id="project-card-bot-lhs" className="lg:w-[30rem] w-full flex justify-center">
 
                         <div id="project-image-carousel-container" className="lg:w-full w-full md:w-3/4">
@@ -44,7 +44,7 @@ const ProjectCard = ({ name, description, imageUrls, tech, wip, soureCodeUrl, ur
                         <div id="project-desc">
                             <div>{description} </div>
                         </div>
-                        <div id="project-card-bot-rhs-bot" className="space-y-6">
+                        <div id="project-card-bot-rhs-bot" className="space-y-6 w-full">
                             <div id="project-links" className="w-full flex flex-row justify-center space-x-4">
                                 <a target="_blank" href={soureCodeUrl} id="project-source" className="bg-blue-500/70 px-5 py-3 rounded-2xl active:bg-light-acc hover:bg-blue-400/70">
                                     Code
@@ -56,7 +56,7 @@ const ProjectCard = ({ name, description, imageUrls, tech, wip, soureCodeUrl, ur
                             <div id="project-stack" className='flex flex-row space-x-4 items-center justify-center'>
                                 <div className="py-4 flex lg:flex-row items-center text-center flex-col justify-center lg:space-x-6 font-extralight flex-wrap bg-white/45 lg:py-2 px-8 rounded-3xl md:space-y-0 space-y-6 space-y-reverse">
                                     <h1 className='lg:pb-0 pb-4'>Stack:</h1>
-                                    <div id="project-skills-grid" className='grid md:grid-cols-3 grid-cols-2 lg:grid-flow-col gap-4 items-center'>
+                                    <div id="project-skills-grid" className='grid md:grid-cols-3 grid-cols-4 lg:grid-flow-col gap-4 items-center'>
                                         {tech.map(technology => <div key={technology.name}>
                                             <Image
                                                 src={technology.imageUrl}
@@ -64,6 +64,7 @@ const ProjectCard = ({ name, description, imageUrls, tech, wip, soureCodeUrl, ur
                                                 width={50} // Set an appropriate width
                                                 height={50} // Set an appropriate height
                                                 className="rounded" // Add Tailwind classes for styling
+                                                title={technology.name}
                                             />
                                         </div>)}
                                     </div>
