dd1ec08a222fe2def788f7971f2fa787d34d89c1
Fatlir Topalli <fatlir-t@outlook.com>
2025-01-02 18:23:59 +0000

    feat: add links pointing to Article page on ArticleCard component.

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface props {
     title: string,
@@ -12,12 +13,12 @@ const ArticleCard = ({ title, author, date, splashImageUrl }: props) => {
     return (
         <>
             <div className={"justify-center flex flex-col text-center items-center"}>
-                <div className="relative aspect-square h-full  w-full flex flex-col justify-center items-center text-center center">
+                <Link href={`/articles/${title}`} className="relative aspect-square h-full  w-full flex flex-col justify-center items-center text-center center">
                     <Image layout="fill" objectFit="cover" src={splashImageUrl} alt={splashImageUrl}>
                     </Image>
-                </div>
+                </Link>
 
-                <div>{title}</div>
+                <Link href={`/articles/${title}`} className="hover:text-my-blue">{title}</Link>
                 <div>{author}</div> . <div>{date}</div>
             </div>
         </>
