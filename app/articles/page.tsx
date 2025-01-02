190b3330011c6a33ff31728123322a07ac424118
Fatlir Topalli <fatlir-t@outlook.com>
2025-01-02 10:05:05 +0000

    feat: improve Articles page layout Change text-alignment for different viewport sizes. Change grid size for different viewport sizes. Increase search-bar size for screens sized small and under. Add gap-y between Articles.

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -10,13 +10,13 @@ export default function Articles() {
 
     return (
         <>
-            <div className='w-3/4 justify-start items-start mb-16 flex flex-row'>
+            <div className='w-full sm:w-3/4 justify-start items-start mb-16 flex flex-row'>
                 <SearchBar></SearchBar>
             </div>
-            <div id="articles" className="w-full flex flex-col mb-16 justify-center text items-center ">
+            <div id="articles" className="w-full flex flex-col mb-16 justify-center text-center md:text-start items-center ">
                 <h1 className='text-4xl font-bold mb-16 drop-shadow-xl w-full'>All Articles</h1>
-                <div className="flex flex-row w-full justify-start items-start">
-                    <div className="text-wrap gap-x-28 grid grid-cols-3 items-center justify-center text-center grid-rows-auto">
+                <div className="flex flex-row w-full justify-center items-center md:justify-start md:items-start">
+                    <div className="text-wrap gap-y-28 gap-x-28 grid md:grid-cols-3 items-center justify-center text-center grid-rows-auto grid-cols-1 ">
                         {
                             articles.map((article) =>
                                 <ArticleCard key={article.title} title={article.title} author={article.author} date={article.date} splashImageUrl={article.splashImageUrl} ></ArticleCard>
