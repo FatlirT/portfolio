baf425e70c2bee0c78f7d83c9a7bcbac12a29dc2
Fatlir Topalli <fatlir-t@outlook.com>
2025-01-15 11:33:46 +0000

    feat: change individual article routes and update links accordingly. To make room for pagination of articles to use the dynamic slug of articles for page number of articles to be displayed.

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -13,12 +13,12 @@ const ArticleCard = ({ title, author, date, splashImageUrl }: props) => {
     return (
         <>
             <div className={"justify-center flex flex-col text-center items-center"}>
-                <Link href={`/articles/${title}`} className="relative aspect-square h-full  w-full flex flex-col justify-center items-center text-center center">
+                <Link href={`/article/${title}`} className="relative aspect-square h-full  w-full flex flex-col justify-center items-center text-center center">
                     <Image layout="fill" objectFit="cover" src={splashImageUrl} alt={splashImageUrl}>
                     </Image>
                 </Link>
 
-                <Link href={`/articles/${title}`} className="hover:text-my-blue">{title}</Link>
+                <Link href={`/article/${title}`} className="hover:text-my-blue">{title}</Link>
                 <div>{author}</div> . <div>{date}</div>
             </div>
         </>
