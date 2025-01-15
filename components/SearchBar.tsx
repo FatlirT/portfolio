add2156a157f37f2e8b4cec9e222b251341546ed
Fatlir Topalli <fatlir-t@outlook.com>
2025-01-15 14:30:11 +0000

    chore: add trial code for touch x scroll for input of search-bar. To be used to trial feature on phone due to lack of emulator.

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -11,7 +11,7 @@ const SearchBar = () => {
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                     </svg>
                 </div>
-                <input id="default-search" className="w-full overflow-scroll text-sm text-white rounded-lg bg-transparent outline-none ring-0 focus:ring-0 focus:outline-none border-none placeholder-white sm:mx-3 mx-2" placeholder="Search Articles" required />
+                <input id="default-search" className="w-full overflow-scroll text-sm text-white rounded-lg bg-transparent outline-none ring-0 focus:ring-0 focus:outline-none border-none placeholder-white sm:mx-3 mx-2 overflow-x-auto whitespace-nowrap touch-pan-x" placeholder="Search Articles" required />
                 <button type="submit" className="text-white bg-dark-acc/30 hover:bg-dark-acc/50 focus:ring-[3px] focus:outline-none focus:ring-light-acc/70 active:bg-dark-acc font-medium rounded-lg text-sm px-4 py-2">Search</button>
             </div>
         </form>
